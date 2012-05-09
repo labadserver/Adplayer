9dbe935a10749d3cebb7d0cab93af60d74d9a8e2
Marius Naumann <marius.naumann@bauermedia.com>
2012-05-09 10:42:30 +0200

    Added simple version map. Added stage-value to .project file.

---
+++ src/js/player/Version.js
@@ -0,0 +1,15 @@
+/**
+ * @name $ADP#version
+ * @field
+ * @description Simple Map which represents the version of the AdPlayer.
+ *
+ * @example
+ * TODO
+ */
+$ADP.version = {
+  major: _@VERSION_MAJOR@_,
+  minor: _@VERSION_MINOR@_,
+  patch: _@VERSION_PATCH@_,
+  stage: '_@VERSION_STAGE@_'
+};
+
