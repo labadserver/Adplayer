8497d370053179066cfe9df2686447ca2ab0e3c3
Marius Naumann <marius.naumann@bauermedia.com>
2012-05-09 11:03:34 +0200

    Turned $ADP.version to function $ADP.getVersion(), which returns a map of version information.

--- src/js/player/Version.js
+++ src/js/player/Version.js
@@ -1,15 +1,29 @@
 /**
  * @name $ADP#version
- * @field
- * @description Simple Map which represents the version of the AdPlayer.
+ * @function
+ * @description Returns a map indicating the current version of the AdPlayer.
+ *              This method should also be used for internal backwards compatibility.
+ *
+ * @returns A map of version information.
  *
  * @example
- * TODO
+ * var version = $ADP.getVersion();
+ * console.log('Major: ' + version.major);
+ * console.log('Minor: ' + version.minor);
+ * console.log('Patch: ' + version.patch);
+ * console.log('Stage: ' + version.stage);
+ * 
+ * // Major: _@VERSION_MAJOR@_
+ * // Minor: _@VERSION_MINOR@_
+ * // Patch: _@VERSION_PATCH@_
+ * // Stage: _@VERSION_STAGE@_
  */
-$ADP.version = {
-  major: _@VERSION_MAJOR@_,
-  minor: _@VERSION_MINOR@_,
-  patch: _@VERSION_PATCH@_,
-  stage: '_@VERSION_STAGE@_'
+$ADP.getVersion = function() {
+  return {
+    major: _@VERSION_MAJOR@_,
+    minor: _@VERSION_MINOR@_,
+    patch: _@VERSION_PATCH@_,
+    stage: '_@VERSION_STAGE@_'
+  }
 };
 
