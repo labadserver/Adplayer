fe54d769092ab7bb0630bb920d1e4c227e76752c
chrissancho <christopher.sancho@adtech.com>
2012-05-09 10:15:31 -0700

    Manual Merge #65 (Added getVersion method to $ADP namespace)

---
+++ src/js/player/Version.js
@@ -0,0 +1,29 @@
+/**
+ * @name $ADP#getVersion
+ * @function
+ * @description Returns a map indicating the current version of the AdPlayer.
+ *              This method should also be used for internal backwards compatibility.
+ *
+ * @returns A map of version information.
+ *
+ * @example
+ * var version = $ADP.getVersion();
+ * console.log('Major: ' + version.major);
+ * console.log('Minor: ' + version.minor);
+ * console.log('Patch: ' + version.patch);
+ * console.log('Stage: ' + version.stage);
+ * 
+ * // Major: _@VERSION_MAJOR@_
+ * // Minor: _@VERSION_MINOR@_
+ * // Patch: _@VERSION_PATCH@_
+ * // Stage: _@VERSION_STAGE@_
+ */
+$ADP.getVersion = function() {
+  return {
+    major: _@VERSION_MAJOR@_,
+    minor: _@VERSION_MINOR@_,
+    patch: _@VERSION_PATCH@_,
+    stage: '_@VERSION_STAGE@_'
+  };
+};
+
